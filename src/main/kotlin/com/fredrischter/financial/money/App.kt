3bac56af1462e77ec274f82384355d5a900fae93
fred <fredrischter@gmail.com>
2019-09-27 08:43:16 +0200

    Testing Spark minimalist HTTP server.

--- src/main/kotlin/com/fredrischter/financial/money/App.kt
+++ src/main/kotlin/com/fredrischter/financial/money/App.kt
@@ -1,5 +1,13 @@
 package com.fredrischter.financial.money
 
+import spark.Spark.*;
+import java.io.*;
+
 fun main(args: Array<String>) {
 	println("Hello World!")
+
+	get("/hello") { req, res ->
+	    "Hello Spark Kotlin"
+	}
+	println("Try http://localhost:4567/hello")
 }
